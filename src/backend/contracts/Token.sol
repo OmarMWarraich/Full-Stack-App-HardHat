2077b754147ffc4619cb827b628a5c3a0d250415
OmarMWarraich <earthling986@gmail.com>
2021-11-06 17:00:30 -0400

    Give totalSupply, ownership and balances to the token

--- src/backend/contracts/Token.sol
+++ src/backend/contracts/Token.sol
@@ -6,4 +6,8 @@ import "hardhat/console.sol";
 contract Token {
   string public name = "Blockchain Master University";
   string public symbol = "BMU";
+
+  uint256 public totalSupply = 1000000;
+  address public owner;
+  mapping(address => uint256) balances;
 }
